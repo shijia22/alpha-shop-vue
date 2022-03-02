07c8a8e795cf95a89324a7c914e2c6eb6b19f099
shijia22 <shichia0207@gmail.com>
2022-03-02 20:22:18 +0800

    feat: 🎸 add buttun style

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="app">
     <h2 class="mb-12">結帳</h2>
-    <div class="left">
-    </div>
-    <div class="right">
-      <ShopCart />
+    <div class="d-flex">
+      <div class="left">
+        <Button />
+      </div>
+      <div class="right">
+        <ShopCart />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import ShopCart from './components/ShopCart.vue'
+import Button from './components/Button.vue'
+
 
 export default {
   name: 'App',
   components: {
     ShopCart,
+    Button,
   },
 }
 </script>
@@ -25,7 +31,6 @@ body {
   font-family: 'Noto Sans TC';
 }
 .app {
-  display: flex;
   max-width: 1440px;
   margin: 0 auto;
   position: relative;
---
+++ src/components/Button.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="btn-container pt-6 d-flex justify-content-between">
+    <button class="back d-flex align-items-center">
+      <img src="/images/left.png" alt="">
+      上一步
+    </button>
+    <button class="next d-flex align-items-center">下一步
+      <img src="/images/right.png" alt="">
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+}
+</script>
+
+<style lang="scss" scoped>
+.btn-container {
+  border-top: 1px solid $simple-gray;
+}
+
+.back {
+  img {
+    width: 1.5rem;
+    height: 1.5rem;
+    margin-left: 0.75rem;
+  }
+}
+
+.next {
+  padding: 11px 48.5px;
+  color: #FFFFFF;
+  border-radius: 8px;
+  background: $btn-color;
+  img {
+    width: 1.5rem;
+    height: 1.5rem;
+    margin-left: 0.75rem;
+  }
+}
+</style>
