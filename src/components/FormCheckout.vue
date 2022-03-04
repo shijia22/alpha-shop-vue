f39871a1db9749c488e79ca74d8f46ac458fbcaf
shijia22 <shichia0207@gmail.com>
2022-03-04 15:45:46 +0800

    feat: 🎸 add FormCheckout style

---
+++ src/components/FormCheckout.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h2 class="mb-8">寄送地址</h2>
+    <div class="form-container">
+      <div class="form-row-only">
+        <div class="form-type">
+          <label for="cc-name">持卡人姓名</label>
+          <input
+            type="cc-name"
+            placeholder="John Doe"
+            class="py-3 pl-4 input-border"
+          />
+        </div>
+      </div>
+      <div class="form-row-only">
+        <div class="form-type">
+          <label for="cc-number">卡號</label>
+          <input
+            type="cc-number"
+            placeholder="1111 2222 3333 4444"
+            class="py-3 pl-4 input-border"
+          />
+        </div>
+      </div>
+      <div class="form-half">
+        <div class="form-type">
+          <label for="cc-number">有效期限</label>
+          <input
+            type="cc-number"
+            placeholder="MM/YY"
+            class="py-3 pl-4 input-border"
+          />
+        </div>
+        <div class="form-type">
+          <label for="cc-csc">CVC/CCV</label>
+          <input type="cc-csc" placeholder="123" class="py-3 pl-4 input-border" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormCheckout',
+}
+</script>
